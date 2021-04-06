33fb8b4846af6af5292e82d3b32ba8c51540c755
SamirFon <samir.mehmedovic@fon.bg.ac.rs>
2021-04-06 15:50:51 +0200

    Napravljena klasa Transakcija sa atributima

---
+++ src/main/java/rs/ac/bg/fon/ai/JSONMenjacnica/Transakcija.java
@@ -0,0 +1,16 @@
+package rs.ac.bg.fon.ai.JSONMenjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Transakcija {
+	
+	private String izvornaValuta ;
+	private String krajnjaValuta; 
+	
+	private double početniIznos;
+	private double konvertovaniIznos;
+	
+	private GregorianCalendar datumTransakcije;
+	
+
+}
